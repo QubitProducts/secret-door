aed566d0d4646daf9b5c69f39b77f0a4ed12c378
Karolis Narkevicius <karolis.n@gmail.com>
2014-11-19 10:46:00 +0000

    Use Firefox for tests to work in Travis

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
       dir : 'coverage/'
     },
 
-    browsers: ['Chrome'],
+    browsers: ['Firefox'],
 
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO
